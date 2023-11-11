0a804e36753d4eaeb27ab21a1f041bafb714caec
iamshoXy <ytshoxy@gmail.com>
2023-11-11 14:43:19 +0100

    Await edit

--- autoreactions/autoreactions.py
+++ autoreactions/autoreactions.py
@@ -14,8 +14,9 @@ class AutoReactions(commands.Cog):
         if isinstance(reaction_channel, discord.TextChannel) and reaction_channel.category_id == int(self.bot.config["main_category_id"]):
             async for message in reaction_channel.history(limit=1, oldest_first=True):  
                 if message.id == reaction.message.id:
+                    print(reaction.emoji)
                     channelName = reaction.emoji + "-" + reaction_channel.name
-                    reaction_channel.edit(name=channelName)
+                    await reaction_channel.edit(name=channelName)
 
 async def setup(bot):
     await bot.add_cog(AutoReactions(bot))
