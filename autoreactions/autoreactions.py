58cc90a1f70c79b6dd2fbcce480beffb16264ec4
iamshoXy <ytshoxy@gmail.com>
2023-11-11 14:16:57 +0100

    Check category id

--- autoreactions/autoreactions.py
+++ autoreactions/autoreactions.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 
 from core import checks
@@ -6,12 +7,13 @@ class AutoReactions(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @checks.thread_only()
     @commands.Cog.listener()
-    async def on_reaction_add(reaction, user):
+    async def on_reaction_add(self, reaction, user):
         reaction_channel = reaction.message.channel
-        messages = await reaction_channel.history(limit=1, oldest_first=True)
-        print(messages)
+
+        if isinstance(reaction_channel, discord.TextChannel) and reaction_channel.category_id == int(self.bot.config["main_category_id"]):
+            messages = await reaction_channel.history(limit=1, oldest_first=True)
+            print(messages)
 
 async def setup(bot):
     await bot.add_cog(AutoReactions(bot))
