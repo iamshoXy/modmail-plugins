c12df2ddce17bec5c40b58141f561cda338f1139
iamshoXy <ytshoxy@gmail.com>
2023-11-11 14:48:35 +0100

    on_reaction_remove

--- autoreactions/autoreactions.py
+++ autoreactions/autoreactions.py
@@ -14,8 +14,17 @@ class AutoReactions(commands.Cog):
         if isinstance(reaction_channel, discord.TextChannel) and reaction_channel.category_id == int(self.bot.config["main_category_id"]):
             async for message in reaction_channel.history(limit=1, oldest_first=True):  
                 if message.id == reaction.message.id:
-                    print(reaction.emoji)
                     channelName = reaction.emoji + "-" + reaction_channel.name
+                    await reaction_channel.edit(name=channelName)
+
+    @commands.Cog.listener()
+    async def on_reaction_remove(self, reaction, user):
+        reaction_channel = reaction.message.channel
+
+        if isinstance(reaction_channel, discord.TextChannel) and reaction_channel.category_id == int(self.bot.config["main_category_id"]):
+            async for message in reaction_channel.history(limit=1, oldest_first=True):  
+                if message.id == reaction.message.id:
+                    channelName = reaction_channel.name.replace(reaction.emoji, '').strip()
                     await reaction_channel.edit(name=channelName)
 
 async def setup(bot):
